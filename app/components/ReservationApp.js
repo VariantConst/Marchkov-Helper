89351ccd2a7b027176aef23a586be025806db7a5
夏一飞 <variantconst@VCsMBA.local>
2024-06-27 01:26:17 +0800

    style(ReservationApp): update main element padding

--- app/components/ReservationApp.js
+++ app/components/ReservationApp.js
@@ -105,7 +105,7 @@ export default function ReservationApp({
         onThemeToggle={toggleTheme}
         theme={theme}
       />
-      <main className="max-w-7xl mx-auto py-2 sm:px-6 lg:px-8">
+      <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         <div className="p-2 sm:px-0">
           <LoadingOverlay isLoading={isLoading} />
           {!user ? (
